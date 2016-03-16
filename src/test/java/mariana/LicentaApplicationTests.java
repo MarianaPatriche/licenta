fdfb34d59caa9ce866b3414b11c212670dbe2b56
mariana.patriche <mariana.patriche@esolutions.ro>
2016-03-16 17:19:22 +0200

    first commit

---
+++ src/test/java/mariana/LicentaApplicationTests.java
@@ -0,0 +1,18 @@
+package mariana;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LicentaApplication.class)
+@WebAppConfiguration
+public class LicentaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
